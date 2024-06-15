887fdf5ae0ddf4b4fe7fd714ceb063417610ba81
MakssMeinarts <makss.meinarts10@28stone.com>
2024-06-15 15:37:37 +0300

    main
    - second solution to challenge 00898-easy-pick.ts

--- playground/easy/00898-easy-includes.ts
+++ playground/easy/00898-easy-includes.ts
@@ -18,9 +18,13 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Includes<T extends readonly any[], U> = {
-  [P in T[number]]: true
-}[U] extends true ? true : false
+type Includes<T extends unknown[], U> =
+  T extends [infer Head, ...infer Tail]
+  ? Equal<U, Head> extends true
+    ? true
+    : Includes<Tail, U>
+  : false
+;
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
